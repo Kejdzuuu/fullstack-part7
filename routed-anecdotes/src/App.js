3e5bda4603f0e84fa985f27bc9dfbc79a2602c8c
Muciej <kejdzuuu@gmail.com>
2021-04-19 23:17:18 +0200

    Ex. 7.5

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -84,6 +84,13 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -101,6 +108,7 @@ const CreateNew = (props) => {
           <input name='info' {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
